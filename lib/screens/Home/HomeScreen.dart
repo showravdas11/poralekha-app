bb49d70cb1d3798498bd7a7db4cd9db4db8521af
uhlapru57 <uhlaprumarma57@gmail.com>
2024-01-26 23:09:13 +0600

    sublist & chaplist done, and homescreen on progress

--- lib/screens/Home/HomeScreen.dart
+++ lib/screens/Home/HomeScreen.dart
@@ -1,12 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:poralekha_app/screens/Home/My_Drawer_Header.dart';
+import 'package:poralekha_app/screens/Home/My_Drawer_list.dart';
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key});
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // Your chat screen content goes here
-      child: const Center(
-        child: Text("Home Screen"),
+    return Scaffold(
+      appBar: AppBar(
+        actions: const [
+          Row(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    'Mina',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+                  ),
+                  Text(
+                    'Class Five',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ],
+              ),
+              SizedBox(width: 10),
+              Padding(
+                padding: EdgeInsets.only(right: 15),
+                child: CircleAvatar(
+                  backgroundImage: AssetImage(
+                    'assets/images/profile.png',
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 200,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.amber,
+                image: const DecorationImage(
+                  image: AssetImage('assets/images/english-banner.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Categories",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "See All",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Color.fromARGB(255, 126, 89, 253),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      drawer: const Drawer(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              MyDrawerHeader(),
+              MyDrawerList(),
+            ],
+          ),
+        ),
       ),
     );
   }
