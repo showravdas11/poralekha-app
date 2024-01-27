212ea2141eda7f131e529d3a11c408c417d292f9
rokydas <droky136@gmail.com>
2024-01-27 19:16:07 +0600

    demo page removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:poralekha_app/screens/Splash/SplashScreen.dart';
-import 'package:poralekha_app/screens/demoPage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
