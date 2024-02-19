db8eb996cb7031eda2c1066cf868aae91b3d992b
showravdas11 <showravdas11@gmail.com>
2024-02-19 18:25:14 +0600

    add chapter done

--- lib/screens/AddChapter/Add_chapter.dart
+++ lib/screens/AddChapter/Add_chapter.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:poralekha_app/common/AppBar.dart';
 import 'package:poralekha_app/common/CommonTextField.dart';
 import 'package:poralekha_app/common/RoundedButton.dart';
@@ -11,7 +12,9 @@ import 'package:poralekha_app/theme/myTheme.dart';
 
 class AddChapterScreen extends StatefulWidget {
   final Map<String, dynamic> subjectData;
-  const AddChapterScreen({Key? key, required this.subjectData})
+  final String docId;
+  const AddChapterScreen(
+      {Key? key, required this.subjectData, required this.docId})
       : super(key: key);
 
   @override
@@ -20,10 +23,10 @@ class AddChapterScreen extends StatefulWidget {
 
 class _AddChapterScreenState extends State<AddChapterScreen> {
   TextEditingController chapnameController = TextEditingController();
-  // String? _filePath;
-  // String? _storagePath;
-  // bool _isExpanded = false;
+  String? _filePath;
   late Stream<QuerySnapshot> _addChapterStream;
+  bool _uploading = false;
+  late File _selectedFile;
 
   @override
   void initState() {
@@ -47,36 +50,73 @@ class _AddChapterScreenState extends State<AddChapterScreen> {
     return downloadLink;
   }
 
-  //------------pick File-----------------
   void pickFile() async {
+    setState(() {
+      _uploading = true;
+    });
+
     final pickedFile = await FilePicker.platform.pickFiles(
       type: FileType.custom,
       allowedExtensions: ['pdf'],
     );
 
     if (pickedFile != null) {
-      String fileName = pickedFile.files[0].name;
+      _selectedFile = File(pickedFile.files[0].path!);
+      setState(() {
+        _filePath = pickedFile.files[0].path;
+        _uploading = false;
+      });
+    } else {
+      setState(() {
+        _uploading = false;
+      });
+    }
+  }
 
-      File file = File(pickedFile.files[0].path!);
-      final downloadLink = await uploadPdf(fileName, file); // Await here
-
-      await _firebasFirestore.collection("pdfs").add(
-        {
-          "name": fileName,
-          "url": downloadLink,
-        },
-      );
+  void addChapter() async {
+    if (chapnameController.text.isEmpty || _selectedFile == null) {
+      // If any required field is empty, show dialog
       AwesomeDialog(
         context: context,
-        dialogType: DialogType.success,
-        animType: AnimType.rightSlide,
-        title: 'Pdf Uploaded SuccessFully',
+        dialogType: DialogType.error,
+        animType: AnimType.bottomSlide,
+        title: 'Enter Required Fields',
+        desc: 'Please fill all the required fields.',
         btnOkColor: MyTheme.buttonColor,
         btnOkOnPress: () {},
       ).show();
-    } else {
-      // User canceled the picker
+      return;
     }
+
+    setState(() {
+      _uploading = true;
+    });
+
+    String fileName = _selectedFile.path.split('/').last;
+    final downloadLink = await uploadPdf(fileName, _selectedFile);
+
+    CollectionReference chapterCollection = FirebaseFirestore.instance
+        .collection('subjects')
+        .doc(widget.docId)
+        .collection('chapters');
+
+    await chapterCollection.add({
+      "name": chapnameController.text,
+      "pdfLink": downloadLink,
+    });
+
+    setState(() {
+      _uploading = false;
+    });
+
+    AwesomeDialog(
+      context: context,
+      dialogType: DialogType.success,
+      animType: AnimType.rightSlide,
+      title: 'Pdf Uploaded Successfully',
+      btnOkColor: MyTheme.buttonColor,
+      btnOkOnPress: () {},
+    ).show();
   }
 
   @override
@@ -143,17 +183,54 @@ class _AddChapterScreenState extends State<AddChapterScreen> {
               text: "Add Chapter Name",
               textInputType: TextInputType.text,
               obscure: false,
+              suffixIcon: Icon(Iconsax.add_circle),
+            ),
+            SizedBox(height: screenSize.height * 0.02),
+            Container(
+              height: 45,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                borderRadius: BorderRadius.circular(6),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    blurRadius: 2,
+                  )
+                ],
+              ),
+              child: TextFormField(
+                onTap: () {
+                  pickFile();
+                },
+                readOnly: true,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: _filePath ?? "Select Your pdf",
+                  hintStyle: TextStyle(
+                    wordSpacing: 2,
+                    letterSpacing: 2,
+                  ),
+                  suffixIcon: _uploading
+                      ? CircularProgressIndicator()
+                      : IconButton(
+                          icon: Icon(Iconsax.attach_circle),
+                          onPressed: pickFile,
+                        ),
+                  alignLabelWithHint: true,
+                  iconColor: Color(0xFF7E59FD),
+                ),
+              ),
             ),
             SizedBox(height: screenHeight * 0.02),
             Center(
               child: RoundedButton(
-                  title: "Add Chapter", onTap: () {}, width: double.infinity),
+                title: "Add Chapter",
+                onTap: addChapter,
+                width: double.infinity,
+              ),
             ),
             SizedBox(height: screenHeight * 0.02),
-            Center(
-              child: RoundedButton(
-                  title: "Upload Pdf", onTap: pickFile, width: double.infinity),
-            ),
           ],
         ),
       ),
