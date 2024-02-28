8308e43be686f001f1ef1a0eef49f7bfc49cc46c
showravdas11 <showravdas11@gmail.com>
2024-02-29 01:06:12 +0600

    user profile upload done

--- lib/screens/UpdateProfileScreen/UpdateProfile.dart
+++ lib/screens/UpdateProfileScreen/UpdateProfile.dart
@@ -4,15 +4,19 @@ import 'dart:io';
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:poralekha_app/common/CommonTextField.dart';
 import 'package:poralekha_app/common/RoundedButton.dart';
 import 'package:poralekha_app/theme/myTheme.dart';
 
 class UpdateProfileScreen extends StatefulWidget {
-  const UpdateProfileScreen({Key? key}) : super(key: key);
+  final Map<String, dynamic> userData;
+  const UpdateProfileScreen({Key? key, required this.userData})
+      : super(key: key);
 
   @override
   State<UpdateProfileScreen> createState() => _UpdateProfileScreenState();
@@ -25,6 +29,7 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
   File? _selectedImage;
   BuildContext? dialogContext;
   String? selectGender;
+  // String? profileImageUrl = widget.userData['profileImageUrl'];
 
   late Stream<QuerySnapshot> _usersStream;
   final auth = FirebaseAuth.instance;
@@ -70,20 +75,27 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
     try {
       User? user = FirebaseAuth.instance.currentUser;
       if (user != null) {
+        // Upload image to Firebase Storage
+        String imagePath = 'profile_images/${user.uid}_profile.jpg';
+        Reference storageReference =
+            FirebaseStorage.instance.ref().child(imagePath);
+        await storageReference.putFile(_selectedImage!);
+
+        // Get the download URL of the uploaded image
+        String imageUrl = await storageReference.getDownloadURL();
+
+        // Update user data in Firebase Realtime Database
         await FirebaseFirestore.instance
             .collection('users')
-            .where('email', isEqualTo: user.email)
-            .get()
-            .then((QuerySnapshot querySnapshot) {
-          querySnapshot.docs.forEach((doc) {
-            doc.reference.update({
-              'name': nameController.text,
-              'gender': selectGender,
-              'address': addressController.text,
-              'age': ageController.text,
-            });
-          });
+            .doc(user.uid)
+            .update({
+          'name': nameController.text,
+          'gender': selectGender,
+          'address': addressController.text,
+          'age': ageController.text,
+          'profileImageUrl': imageUrl, // Store image URL in the database
         });
+
         Navigator.pop(dialogContext!);
         AwesomeDialog(
           context: context,
@@ -110,8 +122,71 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
   //------------------image select form gallery----------------------//
 
   Future<void> _picImageFormGallery() async {
-    final pickedImage =
-        await ImagePicker().pickImage(source: ImageSource.gallery);
+    final picker = ImagePicker();
+    final pickedImage = await showDialog<File?>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Select Image'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                GestureDetector(
+                  child: Row(
+                    children: [
+                      Icon(
+                        Iconsax.gallery_add,
+                        size: 30,
+                        color: Color.fromARGB(255, 142, 36, 171),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        "Gallery",
+                        style: TextStyle(fontSize: 20),
+                      )
+                    ],
+                  ),
+                  onTap: () async {
+                    final pickedFile = await picker.pickImage(
+                      source: ImageSource.gallery,
+                    );
+                    Navigator.of(context).pop(File(pickedFile!.path));
+                  },
+                ),
+                Padding(padding: EdgeInsets.all(8.0)),
+                GestureDetector(
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.camera,
+                        size: 30,
+                        color: Color.fromARGB(255, 48, 96, 78),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        "Camera",
+                        style: TextStyle(fontSize: 20),
+                      )
+                    ],
+                  ),
+                  onTap: () async {
+                    final pickedFile = await picker.pickImage(
+                      source: ImageSource.camera,
+                    );
+                    Navigator.of(context).pop(File(pickedFile!.path));
+                  },
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+
     if (pickedImage != null) {
       setState(() {
         _selectedImage = File(pickedImage.path);
@@ -191,10 +266,16 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
                                   )
                                 : ClipRRect(
                                     borderRadius: BorderRadius.circular(60),
-                                    child: Image.asset(
-                                      "assets/images/person-placeholder.jpg",
-                                      fit: BoxFit.cover,
-                                    ),
+                                    child: widget.userData["profileImageUrl"] !=
+                                            null
+                                        ? Image.network(
+                                            widget.userData["profileImageUrl"]!,
+                                            fit: BoxFit.cover,
+                                          )
+                                        : Image.asset(
+                                            "assets/images/person-placeholder.jpg",
+                                            fit: BoxFit.cover,
+                                          ),
                                   ),
                           ),
                           Container(
